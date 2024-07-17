a71c7e8bf4cdc3442ecaa60f31f5c289c899daba
Rafael Pardim <ppardim27@gmail.com>
2024-07-17 09:38:22 -0300

    Moving pieces

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -45,6 +45,20 @@ public class Board {
 		piece.position = position;
 	}
 	
+	public Piece removerPiece(Position position) {
+		if(!positionExists(position)) {
+			throw new BoardException("Position not on the board");
+		}
+		if (piece(position) == null) {
+			return null;
+		}
+		Piece aux = piece(position);
+		aux.position = null;
+		pieces[position.getRow()][position.getColum()] = null;
+		return aux;
+		
+	}
+	
 	private boolean positionExists(int row, int column) {
 		return row >= 0 && row < rows && column >= 0 && column < columns;
 	}
