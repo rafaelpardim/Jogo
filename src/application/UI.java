968766aac5626c2b7995541caf1cc331fec51bcd
Rafael Pardim <ppardim27@gmail.com>
2024-07-17 09:50:13 -0300

    Handling exceptions and clearing screen

--- src/application/UI.java
+++ src/application/UI.java
@@ -30,19 +30,25 @@ public class UI {
 	public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
 	public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 
+	// https://stackoverflow.com/questions/2979383/java-clear-the-console
+	public static void clearScreen() {
+		System.out.print("\033[H\033[2J");
+		System.out.flush();
+	}
+
 	public static ChessPosition readChessPosition(Scanner sc) {
 		try {
-		String s = sc.nextLine();
-		char column = s.charAt(0);
-		int row = Integer.parseInt(s.substring(1));
-		return new ChessPosition(column, row);
+			String s = sc.nextLine();
+			char column = s.charAt(0);
+			int row = Integer.parseInt(s.substring(1));
+			return new ChessPosition(column, row);
 		}
-		
+
 		catch (RuntimeException e) {
 			throw new InputMismatchException("Erro reading ChessPosition. valid values are from a1 a to h8.");
 		}
 	}
-	
+
 	public static void printBoard(ChessPiece[][] pieces) {
 		for (int i = 0; i < pieces.length; i++) {
 			System.out.print((8 - i) + " ");
@@ -57,12 +63,10 @@ public class UI {
 	private static void printPiece(ChessPiece piece) {
 		if (piece == null) {
 			System.out.print("-");
-		}
-		else {
+		} else {
 			if (piece.getColor() == Color.WHItE) {
 				System.out.print(ANSI_WHITE + piece + ANSI_RESET);
-			}
-			else {
+			} else {
 				System.out.print(ANSI_YELLOW + piece + ANSI_RESET);
 			}
 		}
